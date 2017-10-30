6d29f399b8b65dbed7c78f6910bab6fb3af35e54
phs <jo273175093@qq.com>
2017-10-30 20:01:27 +0800

    完成歌手详情页展示，修复若干bug

--- src/components/singerDetail/singerDetail.vue
+++ src/components/singerDetail/singerDetail.vue
@@ -1,24 +1,40 @@
 <template>
-  <div class="singer-detail" :style="translateY" @touchstart="detailTouchstart" @touchmove="detailTouchmove" ref="singerDetail">
-    <div class="back">
+  <div class="singer-detail" @scroll="playWrapperFixed" ref="singerDetail">
+    <div class="back" @click="back">
       <i class="icon-back"></i>
     </div>
-    <div class="bg-image" :style="bgImage" ref="bgImage">
-      <h1 class="title">{{ detailSinger.name }}</h1>
-      <div class="play-wrapper">
+    <div class="detail-head">
+      <div class="info">
+        <div class="left">
+          <img width="125px" height="125px" :src="this.detailSinger.avatar" alt="">
+        </div>
+        <div class="right">
+          <h1 class="title">{{ detailSinger.name }}</h1>
+          <p class="fans" v-show="fans !== 0">粉丝：{{ formatFans }}万</p>
+          <p class="desc" @click="showMoreTrigger" v-show="singerDesc !== ''">{{ singerDesc }}</p>
+        </div>
+      </div>
+      <div :class="[{playWrapper: true}, {fixed: fixed}]">
         <div class="play">
           <i class="icon-play"></i>
           <span class="text">随机播放全部</span>
         </div>
       </div>
-      <div class="filter" ref="filter"></div>
     </div>
-    <ul class="wrapper" ref="wrapper">
+    <ul class="container">
       <li v-for="(song, index) in songs" :key="index" class="song">
         <h2 class="name">{{ song.name }}</h2>
         <div class="description">{{ `${song.singer}${song.albumdesc ? (' · ' + song.albumdesc) : ''}` }}</div>
       </li>
     </ul>
+    <div class="singerDesc" v-show="singerDescShow">
+      <div class="filter" @touchmove.prevent></div>
+      <div class="main">
+        <p class="text">{{ singerDesc }}</p>
+        <div class="close" @click="showMoreTrigger">关闭</div>
+      </div>
+    </div>
+    <loading v-if="songs.length === 0"></loading>
   </div>
 </template>
 
@@ -26,12 +42,16 @@
 import {getSingerDetail} from 'api/singer.js'
 import {ERR_OK} from 'api/config'
 import {createSong} from 'common/js/song'
+import loading from 'base/loading/loading'
 
 export default {
   data () {
     return {
       songs: [],
-      detailY: 0
+      singerDesc: '',
+      fans: 0,
+      fixed: false,
+      singerDescShow: false
     }
   },
   props: {
@@ -40,11 +60,8 @@ export default {
     }
   },
   computed: {
-    bgImage () {
-      return `background: url(${this.detailSinger.avatar})`
-    },
-    translateY () {
-      return `transform: translateY(${this.detailY}px);`
+    formatFans () {
+      return (this.fans / 10000).toFixed(1)
     }
   },
   created () {
@@ -53,16 +70,13 @@ export default {
     }
     this._getSingerDetail()
   },
-  mounted () {
-    this.maxTranslateY = this.$refs.bgImage.clientHeight - 62 // 可以向上translate的最大距离
-    this.$refs.singerDetail.style.height = `calc(100% + ${this.maxTranslateY}px)`
-    this.$refs.wrapper.addEventListener('scroll', this.preventDefault, false)
-  },
   methods: {
     _getSingerDetail () {
       getSingerDetail(this.$store.state.detailId).then(res => {
         if (res.code === ERR_OK) {
           this.songs = this.normalizeSongData(res.data.list)
+          this.singerDesc = res.data.SingerDesc
+          this.fans = res.data.fans
         }
       })
     },
@@ -73,30 +87,22 @@ export default {
       })
       return ret
     },
-    detailTouchstart (e) {
-      // this.oldPosition = e.changedTouches[0].clientY
+    playWrapperFixed () {
+      if (this.$refs.singerDetail.scrollTop >= 165) {
+        this.fixed = true // 暂时不重构
+      } else {
+        this.fixed = false
+      }
     },
-    detailTouchmove (e) {
-      // let position = e.changedTouches[0].clientY
-      // console.log(position)
-      // let delta = position - this.oldPosition // 两次移动位置的差值
-      // if (delta < 0 && this.detailY >= -this.maxTranslateY) {
-      //   console.log('向上滑')
-      //   // console.log(this.$refs.bgImage.clientHeight)
-      //   // console.log(this.$refs.singerDetail.style.transform)
-      //   console.log(this.$refs.wrapper)
-      //   this.detailY += delta
-      //   return
-      // } else if (delta > 0 && this.detailY < 0) {
-      //   console.log('向下滑')
-      //   this.detailY += delta
-      //   return
-      // }
-      // this.$refs.wrapper.removeEventListener('scroll', this.preventScroll, false)
+    back () {
+      this.$router.push('/singer')
     },
-    preventScroll (e) {
-      e.preventDefault
+    showMoreTrigger () {
+      this.singerDescShow = !this.singerDescShow
     }
+  },
+  components: {
+    loading
   }
 }
 </script>
@@ -106,12 +112,16 @@ export default {
 @import '~common/stylus/mixin.styl'
 
 .singer-detail
-  position: fixed
+  position: absolute // 原本是设置为fixed，这样下面的fixed就会出现抖动的bug，改为absolute就好了
+  margin-top: -88px // 向上覆盖导航栏 
   z-index: 100
   top: 0
   left: 0
   right: 0
+  bottom: 0
   background: $color-background
+  overflow auto
+  -webkit-overflow-scrolling: touch
   .back
     position absolute
     top: 0
@@ -122,30 +132,56 @@ export default {
       padding: 10px
       font-size: $font-size-large-x
       color: $color-theme
-  .title
-    position: absolute
-    top: 0
-    left: 50%
-    transform: translateX(-50%)
-    font-size: $font-size-large
-    color: $color-text
-    margin-top: 15px
-    z-index: 10
-  .bg-image
+  .detail-head
     position: relative
     width: 100%
-    height: 0
-    padding-top: 70%
-    .play-wrapper
-      position: absolute
-      bottom: 15px
-      z-index: 50
+    .info
+      padding: 40px 16px 0 16px
       width: 100%
+      box-sizing: border-box
+      background: #3c3c3c
+      padding-bottom: 60px // 留出空间防止fixed的时候脱离文档流文字向上闪
+      clearfix()
+      .left
+        float: left
+        width: 125px
+        height: 125px
+      .right
+        float: left
+        width: calc(100% - 125px - 10px)
+        margin-top: 20px
+        margin-left: 10px
+        .title
+          vertical-align: top
+          margin-left: 10px
+          font-size: $font-size-large
+          margin-bottom: 18px
+        .fans
+          margin-bottom: 15px
+        .desc
+          no-wrap()
+          width: 100%
+    .playWrapper
+      position: absolute
+      bottom: 0
+      z-index: 200
+      width: 100%
+      height: 60px
+      overflow: hidden
+      background: #3c3c3c
+      transform: translateZ(0)
+      &.fixed
+        position: fixed
+        top: 0
+        left: 0
       .play
+        position: absolute
+        top: 50%
+        left: 50%
+        transform: translate(-50%, -50%)
         box-sizing: border-box
         width: 135px
         padding: 7px 0
-        margin: 0 auto
         text-align: center
         border: 1px solid $color-theme
         color: $color-theme
@@ -160,17 +196,13 @@ export default {
           display: inline-block
           vertical-align: middle
           font-size: $font-size-small
-    .filter
-      position: absolute
-      top: 0
-      left: 0
-      width: 100%
-      height: 100%
-      background: rgba(7, 17, 27, 0.4)
-  .wrapper
+  .container
     margin-left: 20px
+    z-index: inherit // 防止滑动点透滑动bug
     .song
       margin-top: 20px
+      &:last-child
+        margin-bottom: 20px
       .name
         margin-bottom: 10px
         color: $color-text
@@ -178,4 +210,43 @@ export default {
         no-wrap()
         color: $color-text-d
         font-size: $font-size-medium
+  .singerDesc
+    position: fixed
+    top: 0
+    left: 0
+    right: 0
+    bottom: 0
+    z-index: 300
+    .filter
+      background: rgba(7, 17, 27, 0.7)
+      width: 100%
+      height: 100%
+    .main
+      position: absolute
+      top: 50%
+      left: 50%
+      transform: translate(-50%, -50%)
+      width: 80%
+      height: 80%
+      background: $color-background
+      padding-bottom: 50px
+      border-radius: 4px
+      border-top: 4px solid $color-theme
+      border-bottom: 4px solid $color-background // 为了覆盖border-bottom产生的黄色毛边bug
+      box-sizing: border-box
+      overflow: hidden
+      .text
+        overflow: auto
+        -webkit-overflow-scrolling: touch
+        height: 100%
+        padding: 20px
+        padding-bottom: 0
+        box-sizing: border-box
+        color: $color-text-l
+      .close
+        text-align: center
+        height: 40px
+        line-height: 44px
+        border-top: 1px solid rgba(255, 255, 255, 0.23)
+        margin-top: 10px
 </style>
